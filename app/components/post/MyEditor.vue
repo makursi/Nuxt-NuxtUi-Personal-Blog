ecfe566dc28980343f8f9cbcb1041ea3b8e443fb
Choria <2963482580qq.com>
2026-01-27 21:39:07 +0800

    update the createpost page

---
+++ app/components/post/MyEditor.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const value = ref('');
+
+
+</script>
+
+<template>
+  <div>
+    <UEditor v-model="value" class="w-full min-h-21" placeholder="Start writing..." :post-content="value"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- app/pages/admin/create-post.vue
+++ app/pages/admin/create-post.vue
@@ -1,34 +1,66 @@
 <script setup lang="ts">
 const layout = 'admin'
-
-const post = ref({
+import MyEditor from '~/components/post/MyEditor.vue';
+const postInput = ref({
    title:'',
    post_content:'',
-   id:''
+   id:null,
 }) 
+
+defineProps(['postContent']);
+
+postInput.value.post_content = postContent;
+const loading = ref(false)
+
+const createPost = async (req, res)=>{
+   
+  const Post = {
+     title:postInput.value.title,
+     post_content:postInput.value.post_content
+  }
+  try {
+
+    loading.value = true;
+
+    const res = await $fetch(config.public?.API_BASE_URL + "/posts", {
+      headers: {
+        Accept: "application/json",
+        "content-type": "application/json",
+        Authorization: `Bearer ${userData?.token}`,
+      },
+
+      method: "POST",
+      body: JSON.stringify(Post)
+    });
+  } catch (error) {
+    loading.value = false;
+    if (error?.response?.status === 401) {
+      showError(error.response?._data?.message);
+      await navigateTo('/auth/login');
+    }
+  }
+}
+
 </script>
 
 <template>
   <NuxtLayout :name="layout">
    <UContainer>
     <div>
-        <h1>
-          create post
-        </h1>
- 
-
         <UMain>
-          <UFormField>
-            <UInput placeholder="create your post title" v-model="post.title" />
-          </UFormField>
-
-
-
+<div class="p-2 m-2">
+            <UFormField>
+              <UInput placeholder="create your post title" v-model="postInput.title" />
+            </UFormField>
+          </div>
           <ClientOnly>
-
+            <MyEditor />
           </ClientOnly>
-        </UMain>
 
+          <div class="m-2">
+           <UButton @click="createPost">publish post</UButton>
+          </div>
+        </UMain>
       </div>
    </UContainer>
   </NuxtLayout>
